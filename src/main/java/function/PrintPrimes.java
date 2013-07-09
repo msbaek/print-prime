cdec746924f027bf00b65a15506c67b169f03333
msbaek <msbaek@daumcorp.com>
2013-07-09 16:47:54 +0900

    extract method object - NumberPrinter

--- src/main/java/function/PrintPrimes.java
+++ src/main/java/function/PrintPrimes.java
@@ -54,21 +54,39 @@ public class PrintPrimes {
         }
 
         private void printNumbers(int[] numbers, int numberOfPrimes, int linesPerPage, int columns) {
-            int pagenumber = 1;
-            int pageoffset = 1;
-            while (pageoffset <= numberOfPrimes) {
-                System.out.println("The First " + numberOfPrimes +
-                        " Prime Numbers --- Page " + pagenumber);
-                System.out.println("");
-                for (int rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
-                    for (int column = 0; column < columns; column++)
-                        if (rowoffset + column * linesPerPage <= numberOfPrimes)
-                            System.out.format("%10d", numbers[rowoffset + column * linesPerPage]);
+            new NumberPrinter(numbers, numberOfPrimes, linesPerPage, columns).invoke();
+        }
+
+        private class NumberPrinter {
+            private int[] numbers;
+            private int numberOfPrimes;
+            private int linesPerPage;
+            private int columns;
+
+            public NumberPrinter(int[] numbers, int numberOfPrimes, int linesPerPage, int columns) {
+                this.numbers = numbers;
+                this.numberOfPrimes = numberOfPrimes;
+                this.linesPerPage = linesPerPage;
+                this.columns = columns;
+            }
+
+            public void invoke() {
+                int pagenumber = 1;
+                int pageoffset = 1;
+                while (pageoffset <= numberOfPrimes) {
+                    System.out.println("The First " + numberOfPrimes +
+                            " Prime Numbers --- Page " + pagenumber);
                     System.out.println("");
+                    for (int rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
+                        for (int column = 0; column < columns; column++)
+                            if (rowoffset + column * linesPerPage <= numberOfPrimes)
+                                System.out.format("%10d", numbers[rowoffset + column * linesPerPage]);
+                        System.out.println("");
+                    }
+                    System.out.println("\f");
+                    pagenumber = pagenumber + 1;
+                    pageoffset = pageoffset + linesPerPage * columns;
                 }
-                System.out.println("\f");
-                pagenumber = pagenumber + 1;
-                pageoffset = pageoffset + linesPerPage * columns;
             }
         }
     }
