9c5a006e98c9b2d60f7e2f4a810e1787f02e967c
msbaek <msbaek@daumcorp.com>
2013-07-09 16:06:30 +0900

    add test

---
+++ src/test/java/function/PrintPrimesTest.java
@@ -0,0 +1,11 @@
+package function;
+
+import org.junit.Test;
+
+public class PrintPrimesTest {
+    @Test
+    public void
+    should_print_result() {
+        PrintPrimes.main(new String[0]);
+    }
+}
