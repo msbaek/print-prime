1d11d56ae06452a9fc2d4249f261874adf9fa1f5
msbaek <msbaek@daumcorp.com>
2013-07-09 17:06:22 +0900

    Move inner to upper level - PrimeGenerator
    - 이 리팩토링을 수행하기 전에 invoke 메소드의 scope을 public으로 변경한다.

---
+++ src/main/java/function/PrimeGenerator.java
@@ -0,0 +1,49 @@
+package function;
+
+class PrimeGenerator {
+    private final int ordmax = 30;
+    private int[] primes;
+    private int candidate;
+    private int primeIndex;
+    private boolean possiblyPrime;
+    private int ord;
+    private int square;
+    private int n;
+    private int[] multiples;
+
+    public int[] invoke(int numberOfPrimes) {
+        primes = new int[numberOfPrimes + 1];
+        multiples = new int[ordmax + 1];
+        candidate = 1;
+        primeIndex = 1;
+        primes[1] = 2;
+        ord = 2;
+        square = 9;
+        while (primeIndex < numberOfPrimes) {
+            findNextPrime();
+            primeIndex = primeIndex + 1;
+            primes[primeIndex] = candidate;
+        }
+        return primes;
+    }
+
+    private void findNextPrime() {
+        do {
+            candidate = candidate + 2;
+            if (candidate == square) {
+                ord = ord + 1;
+                square = primes[ord] * primes[ord];
+                multiples[ord - 1] = candidate;
+            }
+            n = 2;
+            possiblyPrime = true;
+            while (n < ord && possiblyPrime) {
+                while (multiples[n] < candidate)
+                    multiples[n] = multiples[n] + primes[n] + primes[n];
+                if (multiples[n] == candidate)
+                    possiblyPrime = false;
+                n = n + 1;
+            }
+        } while (!possiblyPrime);
+    }
+}
