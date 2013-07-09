f42ce9dcd3605ba26b92d428b2f4d86741a76553
msbaek <msbaek@daumcorp.com>
2013-07-09 17:05:11 +0900

    extract method - findNextPrime

--- src/main/java/function/PrintPrimes.java
+++ src/main/java/function/PrintPrimes.java
@@ -31,27 +31,31 @@ public class PrintPrimes {
             ord = 2;
             square = 9;
             while (primeIndex < numberOfPrimes) {
-                do {
-                    candidate = candidate + 2;
-                    if (candidate == square) {
-                        ord = ord + 1;
-                        square = primes[ord] * primes[ord];
-                        multiples[ord - 1] = candidate;
-                    }
-                    n = 2;
-                    possiblyPrime = true;
-                    while (n < ord && possiblyPrime) {
-                        while (multiples[n] < candidate)
-                            multiples[n] = multiples[n] + primes[n] + primes[n];
-                        if (multiples[n] == candidate)
-                            possiblyPrime = false;
-                        n = n + 1;
-                    }
-                } while (!possiblyPrime);
+                findNextPrime();
                 primeIndex = primeIndex + 1;
                 primes[primeIndex] = candidate;
             }
             return primes;
+        }
+
+        private void findNextPrime() {
+            do {
+                candidate = candidate + 2;
+                if (candidate == square) {
+                    ord = ord + 1;
+                    square = primes[ord] * primes[ord];
+                    multiples[ord - 1] = candidate;
+                }
+                n = 2;
+                possiblyPrime = true;
+                while (n < ord && possiblyPrime) {
+                    while (multiples[n] < candidate)
+                        multiples[n] = multiples[n] + primes[n] + primes[n];
+                    if (multiples[n] == candidate)
+                        possiblyPrime = false;
+                    n = n + 1;
+                }
+            } while (!possiblyPrime);
         }
     }
 }
